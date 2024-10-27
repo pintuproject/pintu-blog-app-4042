cffa729c43b38fee8f18f2a3a366c0deda092980
Pintu Pradhan <pp154360@gmail.com>
2024-10-27 11:16:09 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
+ 
 app.use(cors());
 app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
