4d48614a8046b01b87a32b6e5b141d91aef4afb3
Pintu Pradhan <pp154360@gmail.com>
2024-10-27 13:41:52 +0530

    package updated

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,11 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
+const corsId={
+  origin:"http://localhost:3000"
+}
  
-app.use(cors());
+app.use(cors(corsId));
 app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
